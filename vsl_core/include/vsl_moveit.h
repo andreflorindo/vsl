7fbbdb1b4d19c4e3e961c4cebb737e6081cd2337
Andre Florindo <andreflorindo@hotmail.com>
2019-08-04 18:40:11 +0200

    iles

---
+++ vsl_core/include/vsl_moveit.h
@@ -0,0 +1,12 @@
+#include <ros/ros.h>
+#include <fstream>
+#include <actionlib/client/simple_action_client.h>
+#include <moveit_msgs/ExecuteTrajectoryAction.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <descartes_trajectory/axial_symmetric_pt.h>
+#include <descartes_trajectory/cart_trajectory_pt.h>
+#include <descartes_planner/dense_planner.h>
+#include <descartes_planner/sparse_planner.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <eigen_conversions/eigen_msg.h>
+#include <ur5_demo_descartes/ur5_robot_model.h>
---
+++ vsl_core/src/read_data.cpp
@@ -0,0 +1,24 @@
+bool getFileContent(std::string fileName, std::vector<double> & path)
+{
+	// Open the File
+	std::ifstream path_file(fileName.c_str());
+ 
+	// Check if object is valid
+	if(!path_file)
+	{
+		std::cerr << "Program could not open the file: "<<fileName<<std::endl;
+		return false;
+	}
+ 
+	std::string str;
+	// Read the next line from File until it reaches the end.
+	while (std::getline(path_file, str))
+	{
+		// Line contains string of length > 0 then save it in vector
+		if(str.size() > 0)
+			path.push_back(str);
+	}
+	//Close The File
+	path_file.close();
+	return true;
+}
