f8a05473696b4f500393586c604dfba28553d740
andreflorindo <andreflorindo@hotmail.com>
2019-08-07 16:41:04 +0200

    add file

--- vsl_core/src/main.cpp
+++ vsl_core/src/main.cpp
@@ -1,8 +1,5 @@
 #include <vsl_sreen_window.h>
 
-
-
-
 #include "widgets/setup_assistant_widget.h"
 #include <ros/ros.h>
 #include <QApplication>
@@ -13,54 +10,43 @@
 
 static void siginthandler(int param)
 {
-  QApplication::quit();
+    QApplication::quit();
 }
 
-void usage(boost::program_options::options_description& desc, int exit_code)
+void usage(boost::program_options::options_description &desc, int exit_code)
 {
-  std::cout << desc << std::endl;
-  exit(exit_code);
+    std::cout << desc << std::endl;
+    exit(exit_code);
 }
-
-
-
-
-
 
 int main(int argc, char **argv)
 {
-  // Parse parameters
-  namespace po = boost::program_options;
+    // Parse parameters
+    namespace po = boost::program_options;
 
-  // Declare the supported options
-  po::options_description desc("Allowed options");
-  desc.add_options()("help,h", "Show help message")("debug,g", "Run in debug/test mode")(
-      "urdf_path,u", po::value<std::string>(), "Optional, path to URDF file in ROS package")(
-      "config_pkg,c", po::value<std::string>(), "Optional, pass in existing config package to load");
+    // Declare the supported options
+    po::options_description desc("Allowed options");
+    desc.add_options()("help,h", "Show help message")("debug,g", "Run in debug/test mode")(
+        "urdf_path,u", po::value<std::string>(), "Optional, path to URDF file in ROS package")(
+        "config_pkg,c", po::value<std::string>(), "Optional, pass in existing config package to load");
 
-  // Process options
-  po::variables_map vm;
-  try
-  {
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
+    // Process options
+    po::variables_map vm;
+    try
+    {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
 
-    if (vm.count("help"))
-      usage(desc, 0);
-  }
-  catch (const std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-    usage(desc, 1);
-  }
+        if (vm.count("help"))
+            usage(desc, 0);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        usage(desc, 1);
+    }
 
-
-
-
-
-
-
-    // Start ROS Node                       <----------
+    // Start ROS Node                                            <----------
     ros::init(argc, argv, "moveit_setup_assistant", ros::init_options::NoSigintHandler);
 
     // ROS Spin
